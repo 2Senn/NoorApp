d2661870a471df3206cc45609aeba9cf8dd4366b
Faisal <1senn.kj@gmail.com>
2022-03-13 02:40:39 +0000

    prettier config file

---
+++ Piety-App/prettier.config.js
@@ -0,0 +1,11 @@
+const options={
+  arrowParens: 'avoid',
+  singleQuote: true,
+  backetSpacing: true,
+  endOfLine: '1f',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+module.exports = options
